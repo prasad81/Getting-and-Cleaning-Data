f90c3640175686df22c1880da3579d6a455410f3
=prasad81 <=rajendra.sheela@yahoo.com>
2017-03-17 18:34:51 -0700

    final commit

--- run_analysis.R
+++ run_analysis.R
@@ -91,7 +91,7 @@ names(Data)
 library(plyr);
 Data2<-aggregate(. ~subject + activity, Data, mean)
 Data2<-Data2[order(Data2$subject,Data2$activity),]
-write.table(Data2, file = "tidydata.txt",row.name=FALSE)
+write.table(Data2, file = "tidydata.csv",row.name=FALSE)
  
  
  ## generating codebook md file
